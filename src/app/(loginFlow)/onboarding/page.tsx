c172275e1e41be5513a14a9b6ade13e2fadd1887
inder <inderprbhatti@gmail.com>
2025-04-14 15:15:20 +0530

    fixed the icon and the name on the onboarding page

--- src/app/(loginFlow)/onboarding/page.tsx
+++ src/app/(loginFlow)/onboarding/page.tsx
@@ -1,7 +1,7 @@
-import { GalleryVerticalEnd } from "lucide-react";
 import OnboardingForm from "./onboardingForm";
 import { auth } from "@/auth";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Onboarding() {
   const session = await auth();
@@ -9,12 +9,19 @@ export default async function Onboarding() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
-          </a>
+          <Link
+            href="/"
+            className="flex items-center gap-2 font-medium font-stretch-ultra-expanded"
+          >
+            <Image
+              src="/icon.svg"
+              alt="dev-ixi"
+              width={50}
+              height={50}
+              className="dark:invert"
+            />
+            DEV-IXI
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
